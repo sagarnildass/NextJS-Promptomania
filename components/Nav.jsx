e4f28c272120ad6d5ef6309902c83eb56fb8ef5c
Sagarnil Das <sagarnildass@gmail.com>
2023-07-18 00:47:12 +0530

    finished navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -83,6 +83,18 @@ const Nav = () => {
                 <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
                   My Profile
                 </Link>
+                <Link href="/create-prompt" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn">
+                  Sign Out
+                </button>
               </div>
             )}
           </div>
